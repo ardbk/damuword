79c6e3f016516678c358a0a8a8fd048c028009b8
ardbk__ <aidyn.ardabek@icloud.com>
2022-06-20 21:50:44 +0200

    commit

--- fb_links.js
+++ fb_links.js
@@ -4,6 +4,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-storage.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDL_P3v2GI-T1_yDC5XF8ggakSZklRfZO0",
+    authDomain: "damuword-fdb1b.firebaseapp.com",
+    projectId: "damuword-fdb1b",
+    storageBucket: "damuword-fdb1b.appspot.com",
+    messagingSenderId: "1045885837483",
+    appId: "1:1045885837483:web:61a1eca3e227c4ec4e17ee",
+    measurementId: "G-H9EJWKL9H4"
+};
+
 app = initializeApp(firebaseConfig);
 analytics = getAnalytics(app);
 auth = getAuth(app);
