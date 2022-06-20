ed96edfc11c87d52a226392347fbd95631d47a6e
ardbk__ <aidyn.ardabek@icloud.com>
2022-06-20 21:40:56 +0200

    commit

---
+++ fb_links.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { getAnalytics } from "";
+import { getAnalytics } from "";
+import { getAnalytics } from "";
+
+// app = initializeApp(firebaseConfig);
+// analytics = getAnalytics(app);
+// auth = getAuth(app);
+
+// ="https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js"></script>
+//     <script src="https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js"></script>
+//     <script src="https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js"></script>
+//     <script src="https://www.gstatic.com/firebasejs/9.8.3/firebase-storage.js"></script>
+//     <script src="https://www.gstatic.com/firebasejs/9.8.3/firebase-firestore.js"></script> -->
